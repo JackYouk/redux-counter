50878290c8ce9fa1d98a57f1f9fe37832c96febc
JackYouk <jackyoukstetter1@gmail.com>
2022-09-22 18:31:14 -0700

    added reducers

---
+++ src/reducers/counterReducer.js
@@ -0,0 +1,16 @@
+import { INCREMENT, DECREMENT } from "../actions/actions";
+
+const counterInitialState = {
+    counter: 0,
+}
+
+export const counterReducer = (state = counterInitialState, action) => {
+    switch(action.type) {
+        case INCREMENT:
+            return {...state, counter: state.counter + 1};
+        case DECREMENT:
+            return {...state, counter: state.counter - 1};
+        default:
+            return state;
+    }
+}
