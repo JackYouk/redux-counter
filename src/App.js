9b035f15d26cbe3ee6c0234442da08b8c2d76486
JackYouk <jackyoukstetter1@gmail.com>
2022-09-27 17:42:06 -0700

    finished redux counter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import {useSelector, useDispatch} from 'react-redux';
+import {increment, decrement} from './actions/actions';
 
 
 function App() {
+  const dispatch = useDispatch();
+  const {counter} = useSelector(state => state.counter);
+
   return (
     <div>
+      <h1>Count: {counter}</h1>
+      <button onClick={() => dispatch(increment())}>
+        increment
+      </button>
+      <button onClick={() => dispatch(decrement())}>
+        decrement
+      </button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './App';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
 
